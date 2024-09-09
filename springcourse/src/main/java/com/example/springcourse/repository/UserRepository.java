79a280657e7e87ccbb32a2ceb72d332ffddfad55
Mushegi <93542704+MushegM@users.noreply.github.com>
2024-09-09 13:01:52 +0300

    Add files via upload

---
+++ springcourse/src/main/java/com/example/springcourse/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.springcourse.repository;
+
+import com.example.springcourse.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
---
+++ springcourse/src/main/java/com/example/springcourse/service/UserServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.springcourse.service;
+
+
+import com.example.springcourse.models.User;
+import com.example.springcourse.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+}
